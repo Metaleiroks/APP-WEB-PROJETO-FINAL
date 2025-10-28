40f991ded80d7eebbc6525dcd0087408af1f8d4d
Metaleiroks <kevynpaulo7@gmail.com>
2025-10-28 20:42:36 -0300

    app web consumindo a api projeto final

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import os
-
+import requests
+import base64
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
+
+usuario = "senai"
+senha = "senai123"
+
 
 app = Flask(__name__)
 
@@ -27,6 +32,32 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+
+@app.route('/alunos', methods=['Get','POST'])  
+def alunos():
+
+    if request.method == 'POST':  
+        nome = request.form.get('nome')
+        idade = request.form.get('idade')
+        cpf = request.form.get('cpf')
+        
+
+        api_url = "https://projeto-final-icec.onrender.com/alunos/"
+
+
+        payload = {
+            "id": 0,
+            "nome": nome,
+            "idade": int(idade),
+            "cpf": cpf
+        }
+
+        response = requests.post(api_url, json=payload)
+        print(response.json)
+        
+        return render_template('aluno.html')
+    else:
+        return render_template('aluno.html')
 
 if __name__ == '__main__':
    app.run()
